1f9d4b6bc13a4a6ca5a287a99112da81da749caa
Praiz Benzy <skyflock@yahoo.com>
2021-08-24 12:27:30 +0100

    changes made to tip cal

--- Tip_calculator/script.js
+++ Tip_calculator/script.js
@@ -1,10 +1,13 @@
 let btn = document.getElementById('btn');
 
 btn.addEventListener('click', function(){
-    let billCurrency = document.getElementById('currency').value;
+    
     let billAmount = document.getElementById('bill_amount').value;
     let tipPercent = document.getElementById('tip_percent').value;
     let noPeople = document.getElementById('no_people').value; 
 
     let totalTip = document.getElementById('total_tip').value = billAmount / tipPercent / noPeople;
+    let totalBill = document.getElementById('total_bill').value = billAmount ;
+
+    total_tip.value = totalTip.toFixed(2)
 })
